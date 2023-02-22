7e2be35c7bc04241b7739ae64a1df7d89cb398a5
Hamzx47 <khanazam3324@gmail.com>
2023-02-22 09:07:30 +0500

    lib changed

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -33,7 +33,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("firebase crud"),
+        title: const Text("firebase crud latest"),
       ),
       body: SafeArea(
         child: Padding(
